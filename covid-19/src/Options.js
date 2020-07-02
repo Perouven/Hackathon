97843be075974dc180efdf1de82f11e3ad703941
Perouven <vperouven@gmail.com>
2020-07-03 02:54:54 +0400

    update graph

--- covid-19/src/Options.js
+++ covid-19/src/Options.js
@@ -1,56 +1,127 @@
 import React from 'react';
+import './Options.css';
 import {DropdownButton, MenuItem} from 'react-bootstrap';
-const path1 =require('../src/Screenshot (40).png');
+const path1 =require('../src/ICU_graph.JPG');
 const path2 =require('../src/Screenshot (41).png');
 const path3 = require('../src/Screenshot (43).png');
+const path1b = require('../src/ICU_graph_ML.JPG.png');
+const path2b = require('../src/Screenshot (50).png');
+const path3b = require('../src/Screenshot (52).png');
+
+
 class Option extends React.Component{
    constructor(props){
        super(props);
        this.state={
-           src: ""
+           src: "",
+           graph: ""
        }
-       this.handleClick =this.handleClick.bind(this);
+      this.handleClick =this.handleClick.bind(this);
+      this.changeGraph = this.changeGraph.bind(this);
    }
+
+
+
 handleClick(e){
 console.log(`button 1 clicked`);
-
-const value= `${e.target.name}`;
-console.log(value);
+const target = e.target;
+const value= target.name;
+const number= e.currentTarget.id;
+//console.log(number);
 this.setState((currentState,CurrentProps)=>{
         return{
-            src:value
+            src:value,
+            graph:number
         }
+        
 })
+
+}
+
+changeGraph(e){
+  const value = this.state.graph;
+  this.setState((currentState,curretProps)=>{
+    console.log(value);
+    if(value==='1')
+    return{
+      src:path1b
+    }
+    else if(value==='2')
+    return{
+      src:path2b
+    }
+    else if(value==='3')
+    return{
+      src:path3b
+    }
+    
+  })
 }
  
+
 render(){
     return (
       <div>
 
         <div class="container">
           <div class="row">
-            <div class="col-md-11">
+            <div class="col-md-11" id="dropdown">
               <h1>Graphs for COVID-19</h1>
-              <hr/>
-              <br></br>
-              <DropdownButton bsStyle="info" key="Info" title="Info">
+              <h3>Select a graph:</h3>
+              <DropdownButton class="dropdown" bsStyle="info" key="Info" title="City" >
                 <MenuItem
-                  eventKey="1"
-                  name= {Image}
+                  id="1"
+                  number="1"
+               
+                  name= {path1}
+                >
+                  Toronto
+                </MenuItem>
+                <MenuItem id="2" >RichmondHill</MenuItem>
+                <MenuItem id="3">Markkham</MenuItem>
+                <MenuItem id="4">Mississauga</MenuItem>
+
+                <MenuItem divider />
+
+                <MenuItem eventKey="5">More options...</MenuItem>
+              
+              </DropdownButton>
+              
+              <DropdownButton class="dropdown" bsStyle="info" key="Info" title="Business field:"  >
+                <MenuItem
+                  id="1"
+                >
+                  HealthCare
+                </MenuItem>
+                <MenuItem id="2"  >Finance</MenuItem>
+                <MenuItem id="3">Education</MenuItem>
+                <MenuItem eventKey="4">Commercial Store</MenuItem>
+                <MenuItem eventKey="5">More options...</MenuItem>
+              </DropdownButton>
+             
+             
+
+              <DropdownButton class="dropdown" bsStyle="info" key="Info" title="graph options:"  >
+                <MenuItem
+                  id="1"
                   onClick={this.handleClick}
                   name= {path1}
                 >
-                  Graph1
+                  Icu Beds
                 </MenuItem>
-                <MenuItem eventKey="2" name= {path2} onClick={this.handleClick}>Graph2</MenuItem>
-                <MenuItem eventKey="3" name= {path3}  onClick={this.handleClick}>
-                 Graph3
-                </MenuItem>
-                <MenuItem divider />
-                <MenuItem eventKey="4">More options...</MenuItem>
-              </DropdownButton>
-              <br /> <br /> <br />
+                <MenuItem id="2" name= {path2} onClick={this.handleClick} >Hospitalized</MenuItem>
+                <MenuItem id="3" name= {path3}  onClick={this.handleClick}> Positive cases</MenuItem>
 
+                <MenuItem divider />
+          
+                <MenuItem eventKey="5">More options...</MenuItem>
+                <MenuItem eventKey="6">More options...</MenuItem>
+              </DropdownButton>
+
+        
+             
+              <hr/>
+              <br></br>
               <div>
                 <img src={this.state.src}/>
               </div>
@@ -59,21 +130,22 @@ render(){
             <br /> <br /> <br />
 
             <label>  
-                Label 1
+                 Total population on the region:
                 <input></input>
             </label>
           <br/><br/>
             <label>  
-                Label 2
+                Total infected on the region:
                 <input></input>
             </label>
             <br/><br/>
             <label>  
-                Label 3
+            Average number of patients:
                 <input></input>
             </label>
+            
                 <br/><br/>
-                <button type="submit">Submit</button>
+                <button type="submit" onClick={this.changeGraph}>Update Graph</button>
             </div>
           </div>
         </div>
